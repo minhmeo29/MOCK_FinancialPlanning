a6d1be90cd6f3c1b7c21c3cd03b126296199f312
minhnd299 <MinhNDHE172328@fpt.edu.vn>
2024-11-02 20:42:51 +0700

    Upload First Spring

---
+++ MOCK_CodeSpring/src/main/java/org/example/mock_codespring/MockCodeSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.mock_codespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockCodeSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockCodeSpringApplication.class, args);
+    }
+
+}
---
+++ MOCK_CodeSpring/src/test/java/org/example/mock_codespring/MockCodeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mock_codespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockCodeSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
